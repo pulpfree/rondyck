071b1549ad9f6d3e82773a594ca84cca41f256ea
Ron Dyck <rond@webbtech.net>
2018-01-03 15:02:39 -0500

    core blog functionality

---
+++ src/blog/2018-01-03-deploy-site/index.md
@@ -0,0 +1,19 @@
+---
+title: "Deploying The Blog"
+cover: "https://unsplash.it/400/300/?random?AngelsofMist"
+category: "test1"
+date: "2018-01-03"
+tags:
+    - React
+    - AWS
+---
+# Deploying A Blog On AWS
+
+## Mane oscula anxia
+
+Lorem markdownum alius, **non voluntas supremaque**: canes ager cingere quis;
+rerum? Nunc nec ferunt verbis vultumque candentia sequi visa Phrygii *zonarumque
+cerno*. Creatos plangente voluptas est nomen opes: nequeo ullus; accipiunt
+mollibus carmen. [Contigerant](http://esse-sertis.net/) haec Antium latebris
+manus quamquam Erigdupum virgultis debes. Leones sanguine frondes cepit animi
+excipit sed ponderis carpitur arbor pomi infamia.
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,51 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const IndexPage = () => (
-  <div>
+const IndexPage = ({ data }) => {
+  console.log(data)
+  return (
+    <div>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
-  </div>
-)
+
+    {data.allMarkdownRemark.edges.map(({ node }) => (
+      <div key={node.id}>
+        <Link
+            to={node.fields.slug}
+          >
+          <h3>
+            {node.frontmatter.title}{" "}
+            <span color="#BBB">{node.frontmatter.date}</span>
+          </h3>
+          <p>{node.excerpt}</p>
+        </Link>
+      </div>
+    ))}
+    </div>
+  )
+}
 
 export default IndexPage
+
+export const query = graphql`
+  query MyFilesQuery {
+    allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}) {
+      edges {
+        node {
+          id,
+          frontmatter {
+            title,
+            date
+          }
+          fields {
+            slug
+          },
+          excerpt,
+          timeToRead
+        }
+      }
+    }
+  }
+`
